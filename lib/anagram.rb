c50c6697744d39c7351aeb5fc56fdd28f5152072
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-01 14:57:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+'require pry'
+class Anagram
+  
+  attr_accessor :word
+  
+  #@array = []
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(array)
+
+    array.collect do|arr_word| 
+      arr_word.split(" , ")
+      if arr_word.sort == @word.sort
+        
+    end
+      
+  end 
+    
+  
+end
+  
+  
+  
+end  
+  
+  
